27d072b37155731e6421ffc9afa9e1cbeff5cabd
unknown <josevieira1709@gmail.com>
2022-03-28 13:04:11 -0300

    Posicionando conteúdo base na tela de informações

--- lib/views/helpPage/help_page.dart
+++ lib/views/helpPage/help_page.dart
@@ -8,13 +8,30 @@ class HelpPage extends StatefulWidget {
 }
 
 class _HelpPageState extends State<HelpPage> {
+
+  Widget createExpansionTile(String title, String text){
+    return ExpansionTile(
+      title: Text(title),
+      textColor: const Color.fromRGBO(240, 125, 54, 1.0),
+      iconColor: const Color.fromRGBO(240, 125, 54, 1.0),
+      collapsedTextColor: const Color.fromRGBO(128, 128, 128, 1),
+      collapsedIconColor: const Color.fromRGBO(128, 128, 128, 1),
+      children: <Widget>[
+        ListTile(
+          title: Text(text),
+          textColor: const Color.fromRGBO(128, 128, 128, 1),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 
       // Configurando App Bar
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "Informações",
           style: TextStyle(
             fontSize: 25,
@@ -22,23 +39,102 @@ class _HelpPageState extends State<HelpPage> {
           ),
         ),
         centerTitle: true,
-        backgroundColor: Color.fromRGBO(240, 125, 54, 1.0),
+        backgroundColor: const Color.fromRGBO(240, 125, 54, 1.0),
       ),
 
       //Configurando corpo da página
-      body: Container(
-        child: Center(
-          child: Text(
-            "Informações aqui...",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Color.fromRGBO(240, 125, 54, 1.0),
+      body: Column(
+        children: [
+          Expanded(
+            child:   ScrollConfiguration(
+              behavior: const ScrollBehavior(),
+              child: GlowingOverscrollIndicator(
+                axisDirection: AxisDirection.down,
+                color: const Color.fromRGBO(128, 128, 128, 1),
+                child: ListView(
+                  padding: const EdgeInsets.all(20),
+                  children: [
+
+                    createExpansionTile(
+                        'Como é feita a avaliação?',
+                        'Lorem ipsum ut curabitur ligula nunc faucibus rhoncus vestibulum rutrum sapien varius, gravida rutrum ultrices auctor arcu malesuada auctor sapien sed hac, eleifend consectetur torquent vitae interdum aliquam himenaeos nam fringilla tincidunt. arcu tempus velit justo habitasse ultricies sem orci vivamus bibendum, class lacus magna ad vivamus curae congue sit.',
+                    ),
+
+                    createExpansionTile(
+                      'Como é calculada a nota total?',
+                      'Lorem ipsum ut curabitur ligula nunc faucibus rhoncus vestibulum rutrum sapien varius, gravida rutrum ultrices auctor arcu malesuada auctor sapien sed hac, eleifend consectetur torquent vitae interdum aliquam himenaeos nam fringilla tincidunt. arcu tempus velit justo habitasse ultricies sem orci vivamus bibendum, class lacus magna ad vivamus curae congue sit.',
+                    ),
+
+                    createExpansionTile(
+                      'Como interpretar a avaliação?',
+                      'Lorem ipsum ut curabitur ligula nunc faucibus rhoncus vestibulum rutrum sapien varius, gravida rutrum ultrices auctor arcu malesuada auctor sapien sed hac, eleifend consectetur torquent vitae interdum aliquam himenaeos nam fringilla tincidunt. arcu tempus velit justo habitasse ultricies sem orci vivamus bibendum, class lacus magna ad vivamus curae congue sit.',
+                    ),
+
+                    createExpansionTile(
+                      'Porque informar o nome junto do segmento?',
+                      'Lorem ipsum ut curabitur ligula nunc faucibus rhoncus vestibulum rutrum sapien varius, gravida rutrum ultrices auctor arcu malesuada auctor sapien sed hac, eleifend consectetur torquent vitae interdum aliquam himenaeos nam fringilla tincidunt. arcu tempus velit justo habitasse ultricies sem orci vivamus bibendum, class lacus magna ad vivamus curae congue sit.',
+                    ),
+
+                    createExpansionTile(
+                      'Como essa avaliação pode ser útil?',
+                      'Lorem ipsum ut curabitur ligula nunc faucibus rhoncus vestibulum rutrum sapien varius, gravida rutrum ultrices auctor arcu malesuada auctor sapien sed hac, eleifend consectetur torquent vitae interdum aliquam himenaeos nam fringilla tincidunt. arcu tempus velit justo habitasse ultricies sem orci vivamus bibendum, class lacus magna ad vivamus curae congue sit.',
+                    ),
+
+                    createExpansionTile(
+                      'Por que avaliar o nome da minha marca?',
+                      'Lorem ipsum ut curabitur ligula nunc faucibus rhoncus vestibulum rutrum sapien varius, gravida rutrum ultrices auctor arcu malesuada auctor sapien sed hac, eleifend consectetur torquent vitae interdum aliquam himenaeos nam fringilla tincidunt. arcu tempus velit justo habitasse ultricies sem orci vivamus bibendum, class lacus magna ad vivamus curae congue sit.',
+                    ),
+
+                    createExpansionTile(
+                      'Quem desenvolveu o aplicativo?',
+                      'Lorem ipsum ut curabitur ligula nunc faucibus rhoncus vestibulum rutrum sapien varius, gravida rutrum ultrices auctor arcu malesuada auctor sapien sed hac, eleifend consectetur torquent vitae interdum aliquam himenaeos nam fringilla tincidunt. arcu tempus velit justo habitasse ultricies sem orci vivamus bibendum, class lacus magna ad vivamus curae congue sit.',
+                    ),
+
+                    createExpansionTile(
+                      'Qual o propósito do aplicativo?',
+                      'Lorem ipsum ut curabitur ligula nunc faucibus rhoncus vestibulum rutrum sapien varius, gravida rutrum ultrices auctor arcu malesuada auctor sapien sed hac, eleifend consectetur torquent vitae interdum aliquam himenaeos nam fringilla tincidunt. arcu tempus velit justo habitasse ultricies sem orci vivamus bibendum, class lacus magna ad vivamus curae congue sit.',
+                    ),
+
+                    createExpansionTile(
+                      'Tem alguma sugestão de melhoria para o aplicativo?',
+                      'Lorem ipsum ut curabitur ligula nunc faucibus rhoncus vestibulum rutrum sapien varius, gravida rutrum ultrices auctor arcu malesuada auctor sapien sed hac, eleifend consectetur torquent vitae interdum aliquam himenaeos nam fringilla tincidunt. arcu tempus velit justo habitasse ultricies sem orci vivamus bibendum, class lacus magna ad vivamus curae congue sit.',
+                    ),
+
+                    Container(
+                      padding: EdgeInsets.fromLTRB(25, 40, 25, 0),
+                      child: Column(
+                        children: [
+                          const Text(
+                            "Entre em contato conosco",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromRGBO(240, 125, 54, 1.0),
+                            ),
+                          ),
+
+                          const SizedBox(height: 20,),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("aa"),
+                            ],
+                          ),
+
+                        ],
+                      ),
+                    ),
+
+
+
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
+        ],
       ),
-
     );
   }
 }
