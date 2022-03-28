40a71733bbe3b391b0901ae2ff25bc41c1dc634f
unknown <josevieira1709@gmail.com>
2022-03-27 23:25:24 -0300

    Finalizando tela de Avaliação

--- lib/views/evaluationPage/evaluation_page.dart
+++ lib/views/evaluationPage/evaluation_page.dart
@@ -2,6 +2,7 @@ import 'package:bnaming_app/http/client.dart';
 import 'package:bnaming_app/views/helpPage/help_page.dart';
 import 'package:flutter/material.dart';
 import 'package:mdi/mdi.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class EvaluationPage extends StatefulWidget {
   const EvaluationPage({Key? key}) : super(key: key);
@@ -15,17 +16,93 @@ class _EvaluationPageState extends State<EvaluationPage> {
   late Map _dados;
   final ClientHttp _api = ClientHttp();
 
+  // Função para criar o campo com o gráfico de resultado de cada critério
+  Widget evaluationCriterionChart(BuildContext context, String criterio, int nota){
+
+    // Criando lista base para o gráfico
+    List<charts.Series<int, String>> data = [
+      charts.Series<int, String>(
+        id: criterio,
+        data: [nota],
+        domainFn: (int n,_) => n.toString(),
+        measureFn: (int n, _) => n,
+        colorFn: (_, __) => charts.Color.fromHex(code: '#F27E35'),
+        measureLowerBoundFn: (int n, __) => 0,
+        measureUpperBoundFn: (int n, __) => 5,
+      )
+    ];
+
+    return Container(
+      //color: Colors.blue,
+      padding: EdgeInsets.fromLTRB(25, 0, 25, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    criterio,
+                    style: const TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(240, 125, 54, 1.0),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    nota.toString(),
+                    style: const TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(128, 128, 128, .7),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          Container(
+            height: 70,
+            child: charts.BarChart(
+              data,
+              animate: true,
+              vertical: false,
+              //charts.NumericAxisSpec(showAxisLine: false, renderSpec: charts.NoneRenderSpec()),
+              domainAxis: charts.OrdinalAxisSpec(showAxisLine: false, renderSpec: charts.NoneRenderSpec()),
+              primaryMeasureAxis: charts.NumericAxisSpec(renderSpec: charts.NoneRenderSpec()),
+            ),
+          ),
+
+          Divider(color: const Color.fromRGBO(128, 128, 128, .7),),
+
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
 
     _dados = ModalRoute.of(context)!.settings.arguments as Map;
-    Future<Map> _avaliacao = _api.postAPI(_dados["name"], _dados["segment"]);
+    //Future<Map> _avaliacao = _api.postAPI(_dados["name"], _dados["segment"]);
+    late Map _avaliacao;
 
     //final Future<String> _calculation = Future<String>.delayed(const Duration(seconds: 1000), () => 'Data Loaded');
 
     // Criando WIDGET para condicionar a tela entre, carregando, erro e quando tudo ocorrer bem
     return FutureBuilder(
-      future: _avaliacao, // Setando variável condicional para o fluxo de telas
+      future: _api.postAPI(_dados["name"], _dados["segment"]).then((value) => _avaliacao = value), // Setando variável condicional para o fluxo de telas
       builder: (context, snapshot) {
         late Widget children;
 
@@ -49,7 +126,7 @@ class _EvaluationPageState extends State<EvaluationPage> {
             //Botão flutuante
             floatingActionButton: FloatingActionButton(
               child: const Icon(Mdi.help,size: 30, color: Colors.white,),
-              backgroundColor: Color.fromRGBO(240, 125, 54, 1.0),
+              backgroundColor: const Color.fromRGBO(240, 125, 54, 1.0),
               onPressed: (){
                 Navigator.push(context, MaterialPageRoute(builder: (context) => const HelpPage()));
               },
@@ -61,28 +138,34 @@ class _EvaluationPageState extends State<EvaluationPage> {
                 children:  [
                   // Configurando o cabeçalho estático da página
                   Padding(
-                    padding: EdgeInsets.fromLTRB(25, 25, 25, 10),
+                    padding: const EdgeInsets.fromLTRB(25, 25, 25, 10),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         // Colocando o nome avaliado
-                        Text(
-                          _dados["name"],
-                          style: const TextStyle(
-                            color: Color.fromRGBO(240, 125, 54, 1.0),
-                            fontSize: 35,
-                            fontWeight: FontWeight.w900,
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                          child: Text(
+                            _dados["name"],
+                            style: const TextStyle(
+                              color: Color.fromRGBO(240, 125, 54, 1.0),
+                              fontSize: 35,
+                              fontWeight: FontWeight.w900,
+                            ),
                           ),
                         ),
                         //Espaçamento
                         const SizedBox(height: 5,),
                         // Colocando o segmento do nome avaliado
-                        Text(
-                          _dados["segment"],
-                          style: const TextStyle(
-                            color: Color.fromRGBO(128, 128, 128, .7),
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                          child: Text(
+                            _dados["segment"],
+                            style: const TextStyle(
+                              color: Color.fromRGBO(128, 128, 128, .7),
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                         //Espaçamento
@@ -101,12 +184,23 @@ class _EvaluationPageState extends State<EvaluationPage> {
                       behavior: const ScrollBehavior(),
                       child: GlowingOverscrollIndicator(
                         axisDirection: AxisDirection.down,
-                        color: Color.fromRGBO(128, 128, 128, .7),
+                        color: const Color.fromRGBO(128, 128, 128, .7),
                         child: ListView(
                           children: [
 
+                            SizedBox(height: 5,),
+
                             // Corpo com as avaliações de cada critério
-                            Text("data")
+                            evaluationCriterionChart(context, "Conceito",_avaliacao["conceito"]),
+                            evaluationCriterionChart(context, "Criatividade",_avaliacao["criatividade"]),
+                            evaluationCriterionChart(context, "Grafia",_avaliacao["grafia"]),
+                            evaluationCriterionChart(context, "Memorização",_avaliacao["memorizacao"]),
+                            evaluationCriterionChart(context, "Originalidade",_avaliacao["originalidade"]),
+                            evaluationCriterionChart(context, "Popularidade",_avaliacao["popularidade"]),
+                            evaluationCriterionChart(context, "Potencial",_avaliacao["potencial"]),
+                            evaluationCriterionChart(context, "Pronuncia",_avaliacao["pronuncia"]),
+                            evaluationCriterionChart(context, "Simplicidade",_avaliacao["simplicidade"]),
+                            evaluationCriterionChart(context, "Sonoridade",_avaliacao["sonoridade"]),
 
                           ],
                         ),
@@ -118,7 +212,7 @@ class _EvaluationPageState extends State<EvaluationPage> {
               )
             ),
           );
-          
+
         // Quando há algum erro no carregamento, vamos para essa tela
         } else if (snapshot.hasError){ // Se tiver erro
           print("ERRO");
